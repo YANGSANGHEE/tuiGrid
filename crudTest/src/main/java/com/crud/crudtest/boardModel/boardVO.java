7cc6f3c0d48269d1ef50dd4a02e4189402889d7a
unknown <tkdgmlek567@gmail.com>
2024-03-06 02:46:50 +0900

    하는중

---
+++ crudTest/src/main/java/com/crud/crudtest/boardModel/boardVO.java
@@ -0,0 +1,63 @@
+package com.crud.crudtest.boardModel;
+
+
+public class boardVO {
+	public int seq;
+	public String title;
+	public String content;
+	public String userId;
+	public String userNm;
+	public String regDt;
+	public String updDt;
+	public String delYn;
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserNm() {
+		return userNm;
+	}
+	public void setUserNm(String userNm) {
+		this.userNm = userNm;
+	}
+	public String getRegDt() {
+		return regDt;
+	}
+	public void setRegDt(String regDt) {
+		this.regDt = regDt;
+	}
+	public String getUpdDt() {
+		return updDt;
+	}
+	public void setUpdDt(String updDt) {
+		this.updDt = updDt;
+	}
+	public String getDelYn() {
+		return delYn;
+	}
+	public void setDelYn(String delYn) {
+		this.delYn = delYn;
+	}
+	
+}
